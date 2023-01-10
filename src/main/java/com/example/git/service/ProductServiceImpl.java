fda2cfbb03ff5c9f741266005fc8228a361ddb55
Suraj Singh <kumarsurajsingh93@gmail.com>
2023-01-10 23:33:45 +0530

    Service file modified

--- src/main/java/com/example/git/service/ProductServiceImpl.java
+++ src/main/java/com/example/git/service/ProductServiceImpl.java
@@ -12,6 +12,8 @@ public class ProductServiceImpl implements ProductService{
 
     @Override
     public void addProduct(Product product) {
-
+        Product product3 = new Product();
+        Product product4 = new Product();
+        Product product5 = new Product();
     }
 }
