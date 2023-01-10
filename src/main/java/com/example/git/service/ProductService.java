480dc5011a523df1e5618c36ea463457934340ba
sksingh1993 <54970572+sksingh1993@users.noreply.github.com>
2023-01-10 23:28:55 +0530

    Update ProductService.java

--- src/main/java/com/example/git/service/ProductService.java
+++ src/main/java/com/example/git/service/ProductService.java
@@ -5,4 +5,5 @@ import com.example.git.model.Product;
 public interface ProductService {
     Product getProduct(Long id);
     void addProduct(Product product);
+    Product deleteProduct(Long id);
 }
